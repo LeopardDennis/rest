ef5fd8537d192c8bf39a3ea73ac5a0a510cc4ff9
txw <txwyates@gmail.com>
2017-07-17 15:41:33 +0800

    use guzzlehttp to send htpp request

--- class/Gini/REST.php
+++ class/Gini/REST.php
@@ -1,6 +1,13 @@
 <?php
-
+/**
+ * Created by PhpStorm.
+ * User: tangxiaowen
+ * Date: 17/7/14
+ * Time: 下午3:18
+ */
 namespace Gini;
+
+use GuzzleHttp\Client;
 
 class REST
 {
@@ -10,31 +17,38 @@ class REST
     private $_version;
     private $_name;
     private $_header = [];
-    private $_uniqid = 1;
+    private $_client;
 
-    private static $_RESTs = [];
-    public static function of($name, $cookie = null, $header = []) {
-        if (!self::$_RESTs[$name]) {
-            $conf = \Gini\Config::get('app.rest');
-            $rest = IoC::construct('\Gini\REST', $conf[$name]['url'], $conf[$name]['path'], $conf[$name]['version'], $cookie, $name, $header);
-            self::$_RESTs[$name] = $rest;
-        }
-        return self::$_RESTs[$name];
-    }
 
-    public function __construct($url, $path = null, $version = null, $cookie = null, $name = null, $header = [])
+    public function __construct($url, $path = null, $header = [], $version = null, $name = null)
     {
         $this->_url = $url;
         $this->_path = $path;
-        $this->_version = $_version;
+        $this->_version = $version;
         $this->_name = $name;
-        $this->_cookie = $cookie ?: IoC::construct('\Gini\REST\Cookie');
+
         $this->_header = (array) $header;
+
+        $this->_header['Accept'] = "application/{$this->_url}+json; version={$this->_version}";
+
+        $this->_header['User-Agent'] = $_SERVER['HTTP_USER_AGENT'] ? : 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)';
+
+        $conf = Config::get('app.rest')[$this->_name];
+
+        if ($conf['client_id'] && $conf['client_secret']) {
+            $authorization = 'HMAC '.$conf['client_id'].':'.base64_encode(hash_hmac('sha1', $conf['client_secret'], $conf['client_id']));
+            $this->_header['Authorization'] = $authorization;
+        }
+
+        $this->_client = new Client([
+            'headers' => $this->_header,
+            'cookies' => $this->_cookie
+        ]);
     }
 
     public function __get($name)
     {
-        return IoC::construct('\Gini\REST', $this->_url, $this->_path ? $this->_path.'/'.$name : $name, $this->_version, $this->_cookie, $this->_header);
+        return IoC::construct('\Gini\REST', $this->_url, $this->_path ? $this->_path.'/'.$name : $name, $this->_header, $this->_version);
     }
 
     public function __call($method, $params)
@@ -43,16 +57,18 @@ class REST
             return;
         }
 
+        $path = '';
         if ($this->_path) {
-            $path = '/' . $this->_path;
+            $path .= '/' . $this->_path;
         }
         $path .= '/' . array_shift($params);
-        
+
+
         $restTimeout = Config::get('rest.timeout');
         $timeout = $restTimeout[$path] ?: $restTimeout['default'];
 
         $raw_data = $this->action([
-            'requset' => strtoupper($method),
+            'request' => strtoupper($method),
             'params' => $params[0],
             'path' => $path,
         ], $timeout);
@@ -64,7 +80,7 @@ class REST
             $message = sprintf('remote error: %s', $data['error']['message']);
             $code = $data['error']['code'];
             throw IoC::construct('\Gini\REST\Exception', $message, $code);
-        } 
+        }
         elseif (is_null($data)) {
             $message = sprintf('unknown error with raw data: %s', $raw_data ?: '(null)');
             throw IoC::construct('\Gini\REST\Exception', $message, 404);
@@ -73,102 +89,47 @@ class REST
         return $data;
     }
 
-    public function setHeader(array $header)
-    {
-        // if format is ['xx: xx'], convert it to ['xx' => 'xx']
-        $kh = [];
-        foreach ($header as $k => $h) {
-            if (is_numeric($k)) {
-                list($k, $v) = explode(':', $h, 2);
-                $kh[trim($k)]=trim($v);
-            } else {
-                $kh[$k] = $h;
-            }
-        }
-        $this->_header = array_merge($this->_header, $kh);
-    }
-
     public function action($post_data, $timeout = 5)
     {
-        $cookie_file = $this->_cookie->file;
+        Logger::of('core')->debug('REST => {url}: {data}', ['url' => $this->_url, 'data' => $post_data]);
 
-        $ch = curl_init();
-
-        $this->_header['Accept'] = "application/{$this->_url}+json; version={$this->_version}"; 
-
-        $conf = \Gini\Config::get('app.rest')[$this->_name];
-
-        if ($conf['client_id'] && $conf['client_secret']) {
-            $authorization = 'HMAC '.$conf['client_id'].':'.base64_encode(hash_hmac('sha1', $conf['client_secret'], $conf['client_id']));
-            $this->_header['Authorization'] = $authorization;
-        }
-        
-        // convert to Key: Value format
-        $header = function($array = []) {
-            $response = [];
-            foreach (array_merge($array, $this->_header) as $key => $value) {
-                $response[] = "$key: $value";
-            }
-            return $response;
-        };
-        $query = (string)http_build_query($post_data['params'] ? : []);
-
-        $options = [
-            CURLOPT_COOKIEJAR => $cookie_file,
-            CURLOPT_COOKIEFILE => $cookie_file,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_AUTOREFERER => false,
-            CURLOPT_FOLLOWLOCATION => false,
-            CURLOPT_CONNECTTIMEOUT => $timeout,
-            CURLOPT_TIMEOUT => $timeout,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'] ? : 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)',
-        ];
-        curl_setopt_array($ch, $options);
-
-        switch ($post_data['requset']) {
+        switch ($post_data['request']) {
             case 'PUT':
+                $response = $this->_client->put($this->_url . $post_data['path'], [
+                    'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+                    'timeout' => $timeout,
+                    'form_params' => $post_data['params'] ?: [],
+                ]);
+                break;
             case 'PATCH':
+                $response = $this->_client->patch($this->_url . $post_data['path'], [
+                    'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+                    'timeout' => $timeout,
+                    'form_params' => $post_data['params'] ?: [],
+                ]);
+                break;
             case 'DELETE':
-                $this->_header['Content-Type'] = 'application/x-www-form-urlencoded';
-                curl_setopt_array($ch, [
-                    CURLOPT_URL => $this->_url . '/' . $post_data['path'],
-                    CURLOPT_POSTFIELDS => $query,
-                    CURLOPT_CUSTOMREQUEST => $post_data['requset']
+                $response = $this->_client->delete($this->_url . $post_data['path'], [
+                    'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+                    'timeout' => $timeout,
+                    'form_params' => $post_data['params'] ?: [],
                 ]);
                 break;
             case 'POST':
-                parse_str($query, $output);
-                curl_setopt_array($ch, [
-                    CURLOPT_URL => $this->_url . '/' . $post_data['path'],
-                    CURLOPT_POST => true,
-                    CURLOPT_POSTFIELDS => $output,
+                $response = $this->_client->post($this->_url . $post_data['path'], [
+                    'form_params' => $post_data['params'] ?: [],
+                    'timeout' => $timeout
                 ]);
                 break;
             default:
-                $this->_header['Content-Type'] = 'application/json';
-                curl_setopt_array($ch, [
-                    CURLOPT_CUSTOMREQUEST => $post_data['requset'],
-                    CURLOPT_URL => $this->_url . '/' . $post_data['path'] . '?' . $query,
+                $response = $this->_client->get($this->_url . $post_data['path'], [
+                    'headers' => ['Content-Type' => 'application/json'],
+                    'timeout' => $timeout,
+                    'query' => $post_data['params'] ?: [],
                 ]);
                 break;
         }
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header());
 
-        \Gini\Logger::of('core')->debug('REST => {url}: {data}', ['url' => $this->_url, 'data' => $post_data]);
-
-        $data = curl_exec($ch);
-        $errno = curl_errno($ch);
-        if ($errno) {
-            $message = curl_error($ch);
-            curl_close($ch);
-
-            \Gini\Logger::of('core')->error('REST cURL error: {url}: {message}', ['url' => $this->_url, 'message' => $message]);
-            throw IoC::construct('\Gini\REST\Exception', "transport error: $message", -32300);
-        }
-
-        curl_close($ch);
-
-        return $data;
+        return $response->getBody()->getContents();
     }
 }
